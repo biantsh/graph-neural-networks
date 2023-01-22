76ff7677ea2164c59e164cbe6025f57dd6b2a5f5
Biant Shkololli <biant.shkololli@solaborate.com>
2023-01-22 03:57:59 +0100

    Replace 'used' with 'raised' for brevity

--- exceptions.py
+++ exceptions.py
@@ -1,10 +1,10 @@
 class InvalidGraphException(Exception):
-    """Used when a graph is initialized with improper parameters."""
+    """Raised when a graph is initialized with improper parameters."""
 
 
 class InvalidNodeException(Exception):
-    """Used when a method is called on a node that doesn't exist."""
+    """Raised when a method is called on a node that doesn't exist."""
 
 
 class InvalidMessageException(Exception):
-    """Used when a ProgressBar is initialized with an improper message."""
+    """Raised when a ProgressBar is initialized with an improper message."""
